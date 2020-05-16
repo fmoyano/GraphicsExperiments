3786158093bc234310150777e581011c6cb22e01
Francis <fran.uma@gmail.com>
2020-05-16 14:33:33 +0200

    Added OpenGL 4.6 support

--- BatchingAndInstancing/FrancisECS.cpp
+++ BatchingAndInstancing/FrancisECS.cpp
@@ -181,8 +181,8 @@ void FrancisECS::Init(int width, int height, const char* title)
 		exit(-1);
 	}
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	window = glfwCreateWindow(width, height, title, nullptr, nullptr);
--- SetupOpenGL/main.cpp
+++ SetupOpenGL/main.cpp
@@ -323,8 +323,8 @@ int main()
 		return -1;
 	}
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	auto window = glfwCreateWindow(640, 480, "Hello OpenGL", nullptr, nullptr);
